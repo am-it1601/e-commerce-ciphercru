91a620d8cc4de2b08bab92826c2e7c8e6bbb11c6
sumit1843007 <135812459+mobiledeveloper2023@users.noreply.github.com>
2024-01-11 17:50:00 +0530

    change price into INR

--- src/app/_components/Price/index.tsx
+++ src/app/_components/Price/index.tsx
@@ -17,7 +17,7 @@ export const priceFromJSON = (priceJSON: string, quantity: number = 1, raw?: boo
 
       if (raw) return priceValue.toString()
 
-      price = (priceValue / 100).toLocaleString('en-US', {
+      price = (priceValue / 100).toLocaleString('en-IN', {
         style: 'currency',
         currency: 'INR', // TODO: use `parsed.currency`
       })
